7877cf0b954d2fd4236e741ec0f815a5a59ec7d4
fahimkhan <khanfahim2000@yahoo.com>
2016-04-15 15:32:30 +0530

    Added Session ID for each user And Create Clear button functionality

--- routes/routes.js
+++ routes/routes.js
@@ -9,13 +9,13 @@ module.exports = function(express,app,fs,os,io){
     });
 
 	io.on('connection', function (socket) {
-  		socket.emit('loadingPage', 'User Connected');
+  		socket.emit('loadingPage', 'User with socket ID '+socket.id+' is Connected');
   		socket.on('user', function (data) {
-    		console.log('Server:'+data);
+    		console.log('Socket ID : '+data['socketID']);
   		});
 
   		socket.on('netlist',function(netlistContent){
-			console.log('Server :'+netlistContent);
+			console.log('Server : '+netlistContent);
 			socket.emit('serverMessage','Client Message Received');
 		});	
 	});
