bd6b54fbbabb7acb1e6614c5abcd358add521d61
fahimkhan <khanfahim2000@yahoo.com>
2016-04-18 17:18:32 +0530

    Subject: Storing the netlist Content in a file
    Description: Write an content Area text into a unique file name.
    Also Added deleteNetlist,getSocketID function.

--- routes/routes.js
+++ routes/routes.js
@@ -9,18 +9,51 @@ module.exports = function(express,app,fs,os,io){
     });
 
 	io.on('connection', function (socket) {
+		socketID = getSocketID(socket);
+		fileName = '/tmp/'+socketID+'.cir.out';
   		socket.emit('loadingPage', 'User with socket ID '+socket.id+' is Connected');
+  		
   		socket.on('user', function (data) {
-    		console.log('Socket ID : '+data['socketID']);
+  			console.log('Socket ID : '+data['socketID']);
   		});
 
   		socket.on('netlist',function(netlistContent){
 			console.log('Server : '+netlistContent);
-			socket.emit('serverMessage','Client Message Received');
+			socket.emit('serverMessage','Recived message for client '+socketID);
+			fs.writeFile(fileName, netlistContent, function (err) {
+				if (err){
+					return console.log(err);
+				} 
+  				console.log('File is stored at '+fileName);
+			});
 		});	
+		
+		socket.on('disconnect',function(){
+			console.log("Client "+socket.id+" disconnected");
+			fs.exists(fileName, function(exists) {
+				if (exists) {
+					//Deleting file
+					deleteNetlistFile(fileName);
+				}
+			});
+		});
+
+		function getSocketID(socket){
+			socketID = socket.id;
+			//Removing first two char i.e /# from socket id
+			socketID = socketID.substring(2);
+			return socketID;
+		}
+
+		function deleteNetlistFile(fileName){
+			console.log("Delete File "+fileName)
+   			fs.unlink(fileName, function(err){
+   				if (err) return console.log(err);
+				console.log("success");
+			});
+		}
+
 	});
 
-
-    
 	app.use('/',router);
 }
